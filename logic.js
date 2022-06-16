d248dafb51b2e820be37e324b05ad28e3ffbde57
Borna Radinovic <borna.radinovic@rocketinsights.com>
2022-06-16 04:54:11 +0200

    core

---
+++ logic.js
@@ -0,0 +1,14 @@
+console.log("asdasd");
+const mySiema = new Siema({
+  selector: '.siema',
+  duration: 500,
+  easing: 'ease-out',
+  loop: true
+});
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+setInterval(() => mySiema.next(), 1000)
+prev.addEventListener("click", () => {
+  mySiema.prev();
+});
+next.addEventListener("click", () => mySiema.next());
